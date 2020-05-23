c0f646597bdc04a6c6b2078ac8236d4cbb5bfd16
kozlyakov.m <kozlyakov.m@gmail.com>
2020-05-23 13:06:39 +0700

    Add handler for departments?id=x

--- src/main/kotlin/com/kozlyakov/project/DepartmentServlet.kt
+++ src/main/kotlin/com/kozlyakov/project/DepartmentServlet.kt
@@ -1,11 +1,11 @@
 package com.kozlyakov.project
 
 
-
 import com.google.gson.Gson
 import com.google.inject.Inject
 import com.google.inject.Singleton
 import com.kozlyakov.project.dao.DepartmentDao
+import xyz.sashenka.webapplication.isInteger
 import java.io.IOException
 import javax.servlet.ServletException
 import javax.servlet.http.HttpServlet
@@ -13,16 +13,31 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Singleton
-class DepartmentServlet: HttpServlet() {
-    @Inject lateinit var departmentDao: DepartmentDao
+class DepartmentServlet : HttpServlet() {
+    @Inject
+    lateinit var departmentDao: DepartmentDao
 
-    @Inject lateinit var gson: Gson
+    @Inject
+    lateinit var gson: Gson
 
     @Throws(ServletException::class, IOException::class)
     override fun doGet(request: HttpServletRequest, response: HttpServletResponse) {
-        response.writer.write(gson.toJson(departmentDao.getAll()))
-
+        //response.writer.write(gson.toJson(departmentDao.getAll()))
+        val query = request.queryString
+        when {
+            query.isNullOrEmpty() -> response.writer.write(gson.toJson(departmentDao.getAll()))
+            query.contains("id") -> handleGetRequestWithIdParameter(request, response)
+            else -> response.sendError(404)
+        }
     }
+
+    private fun handleGetRequestWithIdParameter(request: HttpServletRequest, response: HttpServletResponse) {
+        val idParameter = request.getParameter("id")
+        if (!HandleError().sendErrorForIntegerParameterIfIsNeeded(idParameter, response)) {
+            response.writer.write(gson.toJson(departmentDao.findById(idParameter.toInt())))
+        }
+    }
+
 
     @Throws(ServletException::class, IOException::class)
     override fun doPost(request: HttpServletRequest, response: HttpServletResponse) {
