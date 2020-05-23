7fd31a1a1fe397eb7459235787c267322186e980
kozlyakov.m <kozlyakov.m@gmail.com>
2020-05-23 10:08:27 +0700

    Emprty gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+repositories {
+    mavenCentral()
+}
+
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
