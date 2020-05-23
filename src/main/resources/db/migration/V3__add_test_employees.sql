972dfa54e96c94df3728209583ff4761efc3bffd
kozlyakov.m <kozlyakov.m@gmail.com>
2020-05-23 12:37:25 +0700

    Add test Employees

---
+++ src/main/resources/db/migration/V3__add_test_employees.sql
@@ -0,0 +1,9 @@
+insert into employees (id, name, department_id)
+values (1, 'Hank Schrader', 2), (2, 'Tom Hanks', 1);
+
+insert into tel (id, number, employee_id)
+values (1, 1892637, 1),
+    (2, 2134, 1),
+    (3, 1234637, 1),
+    (4, 45637, 2),
+    (5, 8902737, 2)
