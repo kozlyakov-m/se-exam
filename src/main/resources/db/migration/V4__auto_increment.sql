fc03fbcb7040c94b3861a86faee2a52e68c3fe8c
kozlyakov.m <kozlyakov.m@gmail.com>
2020-05-23 16:57:16 +0700

    Make id auto_increment

---
+++ src/main/resources/db/migration/V4__auto_increment.sql
@@ -0,0 +1,3 @@
+alter table departments alter column id int not null auto_increment;
+alter table employees alter column id int not null auto_increment;
+alter table tel alter column id int not null auto_increment;
