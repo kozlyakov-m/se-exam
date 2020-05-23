e3c6bf87091c06b881facd54ead1cb3b8d283693
kozlyakov.m <kozlyakov.m@gmail.com>
2020-05-23 10:45:59 +0700

    Add first dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,37 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 repositories {
+    jcenter()
     mavenCentral()
 }
 
 plugins {
+
     kotlin("jvm") version "1.3.72"
+    war
 }
 
+val staging:Configuration by configurations.creating
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    staging("com.heroku:webapp-runner-main:9.0.31.0")
+
+    implementation("javax.servlet:javax.servlet-api:3.1.0")
+    implementation("com.google.inject.extensions:guice-servlet:4.2.3")
+
+    implementation("com.google.inject:guice:4.2.3")
+    implementation("com.google.inject.extensions:guice-persist:4.2.3")
+
+    implementation("com.h2database:h2:1.4.200")
+
+    implementation("org.hibernate:hibernate-c3p0:5.4.15.Final")
+
+    implementation("org.flywaydb:flyway-core:6.3.2")
+
+    implementation("org.hibernate:hibernate-entitymanager:5.4.15.Final")
+    implementation("org.hibernate.javax.persistence:hibernate-jpa-2.1-api:1.0.2.Final")
+    implementation("org.hibernate:hibernate-core:5.4.15.Final")
 }
 
 val compileKotlin: KotlinCompile by tasks
