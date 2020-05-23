0331a03712f417835823712f2b711f7637b6208b
kozlyakov.m <kozlyakov.m@gmail.com>
2020-05-23 20:51:48 +0700

    Add 'Employees' button

--- src/main/webapp/react/page.js
+++ src/main/webapp/react/page.js
@@ -1,8 +1,16 @@
 
 function DepartmentsButton(props) {
     return (
-        <button onClick={props.onClick} className="btn btn-primary mb-3">
+        <button onClick={props.onClick} className="btn btn-outline-primary mb-3 mr-2">
            Отделы
+        </button>
+    )
+}
+
+function EmployeesButton(props) {
+    return (
+        <button onClick={props.onClick} className="btn btn-outline-primary mb-3 mr-2">
+           Сотрудники
         </button>
     )
 }
@@ -10,20 +18,27 @@ function DepartmentsButton(props) {
 class Page extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            contentType: 'DepartmentsTable'
+        }
         this.departmentsButtonClick = this.departmentsButtonClick.bind(this)
+        this.employeesButtonClick = this.employeesButtonClick.bind(this)
+
     }
 
-//    const departmentsButton = <button onClick={this.departmentsButtonClick}>!!!</button>
-
     departmentsButtonClick(e){
-        this.set
+        this.setState({contentType: 'DepartmentsTable'})
+    }
+    employeesButtonClick(e){
+        this.setState({contentType: 'EmployeesTable'})
     }
 
     render() {
-
+        console.log(this.state.contentType)
         return (
             <div className="containter">
                 <DepartmentsButton onClick={this.departmentsButtonClick} />
+                <EmployeesButton onClick={this.employeesButtonClick} />
                 <Departments />
             </div>
         )
