af00a8d40be07e47bb581f173c37401e5c28d7bf
kozlyakov.m <kozlyakov.m@gmail.com>
2020-05-23 16:55:28 +0700

    Revert "Make id auto_increment"
    This reverts commit 8572b9b4

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,13 +1,13 @@
 create table departments
 (
-    id            int not null auto_increment primary key,
+    id            int not null primary key,
     department    varchar(255),
     tel           int
 );
 
 create table employees
 (
-    id              int not null auto_increment primary key,
+    id              int not null primary key,
     name            varchar(255),
     department_id   int,
     foreign key (department_id) references departments (id)
@@ -15,8 +15,8 @@ create table employees
 
 create table tel
 (
-    id              int not null auto_increment primary key,
+    id              int not null primary key,
     number          int,
     employee_id     int,
-    foreign key (employee_id) references employees (id)
+    foreign key (employee_id ) references employees (id)
 );
